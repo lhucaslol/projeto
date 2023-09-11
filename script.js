14390c09d422c783e812a6d93e3e636bf9732268
Lhucas santos <lhucaslol@outlook.com>
2023-09-11 18:15:08 -0300

    personalizando foto

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/profilepic.jpg")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "src",
+      "https://avatars.githubusercontent.com/u/75709874?s=400&u=9463438891b1746c6610dc79a4147e8a25633054&v=4"
+    )
   }
 }
